5078a59a248d402736c238dd619e9f035e75e58e
Aarón <demenus.dev@gmail.com>
2024-08-26 01:28:00 +0100

    feat: 🎸 First commit!

---
+++ src/pages/index.tsx
@@ -0,0 +1,57 @@
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import Heading from '@theme/Heading';
+
+import styles from './index.module.css';
+
+type SvgType = React.ComponentType<React.ComponentProps<'svg'>>
+//const Svg = require('@site/static/img/undraw_docusaurus_mountain.svg').default
+
+import * as SvgModule from '@site/static/img/zklogo.svg'
+const Svg = SvgModule.default
+
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <div className={styles.titleContainer}>
+          <Svg className={styles.logoSvg} role="img"></Svg>
+
+          <div className={styles.titleLayout}>
+            <Heading as="h1" className="hero__title">
+              {siteConfig.title}
+            </Heading>
+            <p className="hero__subtitle">{siteConfig.tagline}</p>
+          </div>
+
+        </div>
+
+        <div className={styles.buttons}>
+          {/* <Link
+            className="button button--secondary button--lg"
+            to="/docs/intro">
+            Docusaurus Tutorial - 5min ⏱️
+          </Link> */}
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default function Home(): JSX.Element {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <Layout
+      title={`${siteConfig.title} Docs`}
+      description={siteConfig.tagline}>
+      <HomepageHeader />
+      <main>
+        <HomepageFeatures />
+      </main>
+    </Layout>
+  );
+}
