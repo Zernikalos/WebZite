b467ed682b035272451788adfca20f274b34d4d6
Aarón <demenus.dev@gmail.com>
2025-06-18 13:23:27 +0100

    fix: 🐛 Errors on the production build and the styling

--- src/components/CodeWindow/index.tsx
+++ src/components/CodeWindow/index.tsx
@@ -11,7 +11,7 @@ export default function BlazingFast({className, language, code}: BlazingFastProp
   return (
     <div className={`tw:mockup-window tw:border tw:border-gray-500 tw:w-full ${className || ''}`}>
       <CodeBlock
-        className='tw:mb-0 tw:rounded-none'
+        className='tw:mb-0! tw:rounded-none'
         language={language}
         showLineNumbers
 
